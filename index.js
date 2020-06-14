a3de3b835d6e1f6dd71cbd4eda6e7711d2dd5042
pawan <pawan.dharel777@gmail.com>
2020-06-14 10:55:56 +0545

    mongodb connection setup done

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
 const express=require('express')
+const mongoose=require('mongoose')
 const dotenv=require('dotenv').config();
-
 const app=express();
 
+// mongodb connection
+const mongoUri =process.env.MONGODB;
+mongoose.connect(mongoUri, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+  useFindAndModify:false
+});
+
+mongoose.connection.on("connected", () => {
+  console.log("Connected to mongodb");
+});
+
+mongoose.connection.on("error", (err) => {
+  console.log("Unable to connect to mongodb");
+});
+
+
+// server connection
 app.listen(process.env.PORT,()=>{
     console.log(`Connected on port ${process.env.PORT}`)
 })
+
